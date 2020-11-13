8125da26349b4f9b25d427c625dc0798dc15ec86
lisonggen <Lsg050828>
2020-11-13 23:17:33 +0800

    zuul

--- architecture-zuul/src/main/java/com/lisg/zuul/MainConfig.java
+++ architecture-zuul/src/main/java/com/lisg/zuul/MainConfig.java
@@ -11,36 +11,36 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration		// 让spring boot 项目启动时识别当前配置类
-@ComponentScan({"com.lisg.*"})	//
+@Configuration        // 让spring boot 项目启动时识别当前配置类
+@ComponentScan({"com.lisg.*"})    //
 public class MainConfig {
-	
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(redisConnectionFactory);
- 
-		// 使用Jackson2JsonRedisSerialize 替换默认序列化
-		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
- 
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
- 
-		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
- 
-		// 设置value的序列化规则和 key的序列化规则
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-		
- 
-		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-		redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
- 
-		redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
-		redisTemplate.setEnableDefaultSerializer(true);
-		redisTemplate.afterPropertiesSet();
-		return redisTemplate;
-	}
-	
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setEnableDefaultSerializer(true);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
 }
--- architecture-zuul/src/main/java/com/lisg/zuul/filter/MyZuulFilter.java
+++ architecture-zuul/src/main/java/com/lisg/zuul/filter/MyZuulFilter.java
@@ -19,101 +19,101 @@ import java.util.Date;
 @Component
 public class MyZuulFilter extends ZuulFilter {
 
-	@Value("${zuul.authFilter.ignoreUrl}")
-	private String[] ignoreUrl;
+    @Value("${zuul.authFilter.ignoreUrl}")
+    private String[] ignoreUrl;
 
-	@Resource
-	private RedisTemplate<String, Object> redisTemplate;
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
-	/**
-	 * 表示当前的过滤器是否被调用
-	 */
-	@Override
-	public boolean shouldFilter() {
-		return true;
-	}
+    /**
+     * 表示当前的过滤器是否被调用
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
 
-	/**
-	 * filterOrder表示执行的优先级，值越小表示优先级越高
-	 */
-	@Override
-	public int filterOrder() {
-		return 0;
-	}
+    /**
+     * filterOrder表示执行的优先级，值越小表示优先级越高
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
 
-	/**
-	 *  pre:		在请求被路由之前调用
-		routing: 	在请求被路由之中调用
-		post: 		在请求被路由之后调用
-		error: 		处理请求发生错误时调用
-	 */
-	@Override
-	public String filterType() {
-		return "pre";
-	}
-	
-	/**
-	 * 真正执行Filter逻辑的方法
-	 */
-	@Override
-	public Object run() throws ZuulException {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		HttpServletRequest request = ctx.getRequest();
-		//1:获取请求携带的token
-		String token = request.getHeader("x-auth-token");
-		String url = request.getRequestURI();
+    /**
+     * pre:		在请求被路由之前调用
+     * routing: 	在请求被路由之中调用
+     * post: 		在请求被路由之后调用
+     * error: 		处理请求发生错误时调用
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
 
-		//2:判断请求是否需要登录验证
-		boolean needCheck = needCheck(url);
-		if (needCheck) {
-			//token为空
-			if (StringUtils.isBlank(token)) {
-				setResponseErrorInfo(ctx, 400, "token is empty!");
-			} else {
-				//获取userId
-				String userId = request.getParameter("userId");
-				//通过userId，从redis获取token,和请求端携带的token进行比较
-				HashOperations<String, Object, Object> hoHash = redisTemplate.opsForHash();
-				String cacheKey = "x-token" + ":" + userId;
-				String cacheToken = (String) hoHash.get("chengdu:userToken", cacheKey);
+    /**
+     * 真正执行Filter逻辑的方法
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        //1:获取请求携带的token
+        String token = request.getHeader("x-auth-token");
+        String url = request.getRequestURI();
 
-				if (StringUtils.isNotBlank(cacheToken)) {
-					//拆分cacheToken,比较token是否相等，判断是否过时
-					String[] tokens = cacheToken.split(":");
-					String nowTime = DateFormatUtils.format(new Date(), "yyyyMMddHHmmss");
-					if (!token.equals(tokens[0])) {
-						setResponseErrorInfo(ctx, 401, "token is not correct!");
-					} else if (Long.parseLong(nowTime) > Long.parseLong(tokens[1])) {
-						setResponseErrorInfo(ctx, 402, "token is expired!");
-						//如果token过期，则删掉
-						hoHash.delete("chengdu:userToken", "x-token" + ":" + userId);
-					}
-				} else {
-					setResponseErrorInfo(ctx, 403, "please login!");
-				}
-			}
-		}
-		
-		return ctx;
-	}
+        //2:判断请求是否需要登录验证
+        boolean needCheck = needCheck(url);
+        if (needCheck) {
+            //token为空
+            if (StringUtils.isBlank(token)) {
+                setResponseErrorInfo(ctx, 400, "token is empty!");
+            } else {
+                //获取userId
+                String userId = request.getParameter("userId");
+                //通过userId，从redis获取token,和请求端携带的token进行比较
+                HashOperations<String, Object, Object> hoHash = redisTemplate.opsForHash();
+                String cacheKey = "x-token" + ":" + userId;
+                String cacheToken = (String) hoHash.get("chengdu:userToken", cacheKey);
 
-	//判断一个请求，是否需要登录验证
-	public boolean needCheck(String url) {
-		boolean needCheck = true;
-		for (String igoUrl : ignoreUrl) {
-			if (url.startsWith(igoUrl)) {
-				return false;
-			}
-			break;
-		}
-		return  needCheck;
-	}
+                if (StringUtils.isNotBlank(cacheToken)) {
+                    //拆分cacheToken,比较token是否相等，判断是否过时
+                    String[] tokens = cacheToken.split(":");
+                    String nowTime = DateFormatUtils.format(new Date(), "yyyyMMddHHmmss");
+                    if (!token.equals(tokens[0])) {
+                        setResponseErrorInfo(ctx, 401, "token is not correct!");
+                    } else if (Long.parseLong(nowTime) > Long.parseLong(tokens[1])) {
+                        setResponseErrorInfo(ctx, 402, "token is expired!");
+                        //如果token过期，则删掉
+                        hoHash.delete("chengdu:userToken", "x-token" + ":" + userId);
+                    }
+                } else {
+                    setResponseErrorInfo(ctx, 403, "please login!");
+                }
+            }
+        }
 
-	//设置返回结果
-	private void setResponseErrorInfo(RequestContext ctx, int responseStatusCode, String responseBody){
-		ctx.setSendZuulResponse(false);
-		ctx.setResponseStatusCode(responseStatusCode);
-		ctx.setResponseBody(responseBody);
-	}
+        return ctx;
+    }
+
+    //判断一个请求，是否需要登录验证
+    public boolean needCheck(String url) {
+        boolean needCheck = true;
+        for (String igoUrl : ignoreUrl) {
+            if (url.startsWith(igoUrl)) {
+                return false;
+            }
+            break;
+        }
+        return needCheck;
+    }
+
+    //设置返回结果
+    private void setResponseErrorInfo(RequestContext ctx, int responseStatusCode, String responseBody) {
+        ctx.setSendZuulResponse(false);
+        ctx.setResponseStatusCode(responseStatusCode);
+        ctx.setResponseBody(responseBody);
+    }
 
 }
